f890e7fe5e53cd2cb36d3c07e2039e58af98c0f5
kbaikov <konstantin.baikov@gmail.com>
2022-12-01 22:35:48 +0100

    Solve day 01

---
+++ day01/part2.py
@@ -0,0 +1,56 @@
+import argparse
+from pathlib import Path
+import pytest
+
+
+INPUT_TXT = Path("input.txt")
+
+
+def compute(s: str) -> int:
+    elves = s.strip().split("\n\n")
+    calories = []
+    for elf_numbers in elves:
+        calories.append(sum(int(number.strip()) for number in elf_numbers.splitlines()))
+    return sum(sorted(calories, reverse=True)[:3])
+
+
+INPUT_S = """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+EXPECTED = 45000
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file, encoding="utf-8") as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
